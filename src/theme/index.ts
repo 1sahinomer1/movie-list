03f4ed56e9876057c9c69647c17602d20d3261ed
Ömer Şahin <63051255+1sahinomer1@users.noreply.github.com>
2021-11-09 15:09:10 +0300

    add responsive breakpoints

---
+++ src/theme/index.ts
@@ -0,0 +1,8 @@
+export const breakpoints = {
+  xs: "(max-width: 320px)",
+  sm: "(max-width: 375px)",
+  md: "(max-width: 425px)",
+  lg: "(max-width: 992px)",
+  xl: "(max-width: 1200px)",
+  xxl: "(max-width: 1440px)",
+};
